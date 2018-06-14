e2b190cf469181c7e9e132d11b9b23edbfee9555
joti sempe <josefinasempe@gmail.com>
2018-06-14 15:34:34 -0300

    (cocktail, ingredient and dose) models and cocktails and doses controllers created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "fernet")
+Ingredient.create(name: "Light rum")
+Ingredient.create(name: "Applejack")
+Ingredient.create(name: "Gin")
+Ingredient.create(name: "Sweet Vermouth")
+Ingredient.create(name: "Strawberry schnapps")
+Ingredient.create(name: "Scotch")
+Ingredient.create(name: "Orange bitters")
+Ingredient.create(name: "Vodka")
+Ingredient.create(name: "Champagne")
+Ingredient.create(name: "Tequila")
